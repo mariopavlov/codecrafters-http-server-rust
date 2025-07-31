ca63e9d5eebae8e6a4499bf4be6645011b3fbe6f
Mario Pavlov <marioppavlov@gmail.com>
2025-07-31 13:57:43 +0300

    Handle Client Request
    Return HTTP 200 Status Code to all clients

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-#[allow(unused_imports)]
 use std::net::TcpListener;
+use std::{io::Write, net::TcpStream};
+
+fn handle_client(mut stream: TcpStream) {
+    println!("accepted new connection");
+    let status = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(status.as_bytes()).unwrap();
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,7 +17,7 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+                handle_client(_stream);
             }
             Err(e) => {
                 println!("error: {e}");
